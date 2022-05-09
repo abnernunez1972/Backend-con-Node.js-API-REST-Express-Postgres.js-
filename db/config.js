e240d074b234547a7e6a91d4db6d1d04ca84ba6a
abnernunez1972 <abner.nunez1972@gmail.com>
2022-05-08 19:31:19 -0500

    Study Session 08-05-22 7

--- db/config.js
+++ db/config.js
@@ -2,15 +2,15 @@ const { config } = require('./../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
     url: URI,
-    dialect: 'postgres',
+    dialect: 'mysql',
   },
   production: {
     url: URI,
-    dialect: 'postgres',
+    dialect: 'mysql',
   }
 }
